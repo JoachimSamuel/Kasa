42ec951f1c0bf1d486ef23f9b4480176f673adf4
Joachim Samuel <joachimsamuel@gmail.com>
2023-07-05 22:00:41 +0200

    Ajout du crrousel plus liste

--- tuto__react/src/Components/Logement/FicheLogement.js
+++ tuto__react/src/Components/Logement/FicheLogement.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Carrousel from "./carrousel";
 import MyDropdown from "./dropdown";
+
 
 export default function FicheLogement() {
     return (
          <div>
             <h1>Fiche Logement!</h1>
+            <Carrousel />
             <MyDropdown />
         </div>
     )
---
+++ tuto__react/src/Components/Logement/carrousel.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Carrousel = () => {
+  return (
+    <div className="carrousel">
+      {/* Contenu du carrousel */}
+      <img src="" alt="" />
+      <img src="" alt="" />
+      <img src="" alt="" />
+    </div>
+  );
+};
+
+export default Carrousel;
