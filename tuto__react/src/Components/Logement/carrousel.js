2a199aed8e9d48832fbee6493e4dc6aad763793a
Joachim Samuel <joachimsamuel@gmail.com>
2023-07-06 19:21:08 +0200

    Carrousel OK

--- tuto__react/src/Components/Logement/carrousel.js
+++ tuto__react/src/Components/Logement/carrousel.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from 'react';
 
 const Carrousel = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const images = [
+    { src: 'image1.jpg', alt: 'Image 1' },
+    { src: 'image2.jpg', alt: 'Image 2' },
+    { src: 'image3.jpg', alt: 'Image 3' },
+  ];
+
+  const nextImage = () => {
+    setCurrentImage((prevImage) => (prevImage === images.length - 1 ? 0 : prevImage + 1));
+  };
+
+  const prevImage = () => {
+    setCurrentImage((prevImage) => (prevImage === 0 ? images.length - 1 : prevImage - 1));
+  };
+
   return (
     <div className="carrousel">
-      {/* Contenu du carrousel */}
-      <img src="" alt="Image 1" />
-      <img src="" alt="Image 2" />
-      <img src="" alt="Image 3" />
+      {/* Bouton "Précédent" */}
+      <button className="prev-btn" onClick={prevImage}>
+        &#8249;
+      </button>
+
+      {/* Image courante */}
+      <img className="carrousel-image" src={images[currentImage].src} alt={images[currentImage].alt} />
+
+      {/* Bouton "Suivant" */}
+      <button className="next-btn" onClick={nextImage}>
+        &#8250;
+      </button>
     </div>
   );
 };
