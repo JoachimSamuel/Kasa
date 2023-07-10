2014a06e9e0d4196d485b4a1306ea74cf0976f7f
Joachim Samuel <joachimsamuel@gmail.com>
2023-07-10 14:04:48 +0200

    Ajout des photo sur la page Home

--- tuto__react/src/Components/Home/Home.js
+++ tuto__react/src/Components/Home/Home.js
@@ -1,23 +1,34 @@
-import React from "react";
-import Carte from './carte'; 
+import React, { useEffect, useState } from "react";
+import Carte from "./carte";
 import Banner from "../banner";
 
-const Home = () => {
+function Home() {
+  const [cartesData, setCartesData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/data/data.json');
+        const data = await response.json();
+        setCartesData(data);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des données :', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
       <Banner />
       <div className="contenair_carte">
-      <Carte /> {/* Utiliser le composant Carte avec une majuscule */}
-      <Carte /> {/* Utiliser le composant Carte avec une majuscule */}
-      <Carte /> {/* Utiliser le composant Carte avec une majuscule */}
-      <Carte /> {/* Utiliser le composant Carte avec une majuscule */}
-      <Carte /> {/* Utiliser le composant Carte avec une majuscule */}
-      <Carte /> {/* Utiliser le composant Carte avec une majuscule */}
-      <Carte /> {/* Utiliser le composant Carte avec une majuscule */}
-      <Carte /> {/* Utiliser le composant Carte avec une majuscule */}
+      {cartesData.map((carte) => (
+        <Carte key={carte.id} title={carte.title} cover={carte.cover} />
+      ))}
       </div>
     </div>
   );
-};
+}
 
 export default Home;
--- tuto__react/src/Components/Home/carte.js
+++ tuto__react/src/Components/Home/carte.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
-function Carte() {
+function Carte( { title, cover } ) {
   return (
     <Link to="/fiche-logement">
         <div className="carte">
-            <img src="" alt="Image de la location" />
-            <span className="carte-titre">Titre de la location</span>
+            <img src={cover} alt="Image de la location" />
+            <span className="carte-titre">{title}</span>
         </div>
     </Link>
   );
