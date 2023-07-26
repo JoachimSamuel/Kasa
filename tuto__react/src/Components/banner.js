6af78d9816232f80a8c586d5b4256190a4a2d61c
Joachim Samuel <joachimsamuel@gmail.com>
2023-07-27 00:07:33 +0200

    Default dropDown Ok

--- tuto__react/src/Components/banner.js
+++ tuto__react/src/Components/banner.js
@@ -10,7 +10,8 @@ function Banner() {
       {isHomePage ? (
         <>
           <img src={img1} alt="rocher " />
-          <h1>Chez vous, partout et ailleurs</h1>
+          <h1>Chez vous, <br />
+           partout et ailleurs</h1>
         </>
       ) : (
         <img src={img2} alt="rocher " />
